7d21b8fe0c376ce735866cf1a8d47615d9665783
amitvchaudhary <amit.chaudhary140@gmail.com>
2017-02-19 12:53:43 +0530

    Modified Release Target as drop down
    Changed the filed type from drop down to select because it will get a
    reference from the release table.

--- app/scripts/controllers/newApp.js
+++ app/scripts/controllers/newApp.js
@@ -10,13 +10,20 @@
 angular.module('aobappApp')
   .controller('newAppCtrl', function($scope) {
     
+    $scope.selectedPriority="Low";
+    $scope.partOf="BAU";
+    $scope.type="SOX";
+  
     
-    $scope.cars = {
+    $scope.priorities = {
         Low : "Low",
         Medium : "Medium",
         High : "High"
     };
     
+    $scope.releases=['Jan17','Feb17','Mar17'];
+    
+    $scope.selectedRelease=$scope.releases[0];
     
     
   });
